7b54ed7e06a316ed976c3b4581b6511445f5f474
Matteo Defranchis <matteo.defranchis@cern.ch>
2024-08-01 12:11:05 +0200

    smearing and morphing

---
+++ fitUtils.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import pandas as pd
+import scipy.ndimage
+
+def getMaxXsec(df_xsec, range_max_ecm = None, step = 1):
+    if range_max_ecm is None:
+        range_max_ecm = df_xsec['ecm'].max()
+    max_xsec = df_xsec[df_xsec['ecm'] <= range_max_ecm]['xsec'].max()
+    max_ecm = df_xsec.loc[df_xsec['xsec'] == max_xsec, 'ecm'].values[0]
+    if max_ecm > range_max_ecm - step/2:
+        return getMaxXsec(df_xsec, range_max_ecm - step, step)
+    return max_xsec, max_ecm
+
+def convoluteXsecGauss(df_xsec, sqrt_res):
+    tolerance = 6 # number of decimal places to consider
+    pitch = round(df_xsec['ecm'][1] - df_xsec['ecm'][0],tolerance)
+    pitches = [round(df_xsec['ecm'][i+1] - df_xsec['ecm'][i],tolerance) for i in range(len(df_xsec['ecm'])-1)]
+    if not (pitches == pitch).all():
+        raise ValueError('Non-uniform pitch')
+    _ , max_ecm = getMaxXsec(df_xsec)
+    sigma = max_ecm *sqrt_res*(2**.5)/100/pitch
+    xsec_smear = scipy.ndimage.gaussian_filter1d(df_xsec['xsec'], sigma)
+    df_xsec_smear = pd.DataFrame({'ecm': df_xsec['ecm'], 'xsec': xsec_smear})
+    return df_xsec_smear
--- makeFit.py
+++ makeFit.py
@@ -1,21 +1,28 @@
 import numpy as np
 import pandas as pd
-import iminuit
-import os
-import argparse
+import iminuit, scipy
+import os, argparse
 import matplotlib.pyplot as plt
+from fitUtils import convoluteXsecGauss
 
 indir = 'output_ISR/for_fit'
 parameters = ['mass','width','yukawa','as']
 
+def formTag(mass, width, yukawa, alphas):
+    return 'mass{:.2f}_width{:.2f}_yukawa{:.1f}_as{}'.format(mass,width,yukawa,alphas)
+
 class fit:
-    def __init__(self,input_dir,parameters) -> None:
+    def __init__(self, input_dir, parameters, beam_energy_res = 0.221, smearXsec = True) -> None:
         self.input_dir = input_dir
         self.params = parameters
+        self.beam_energy_res = beam_energy_res
+        self.smearXsec = smearXsec
         self.fetchInputs()
         self.checkParameterNames()
         self.getParameterValues()
         self.readCrossSections()
+        self.smearCrossSections()
+        self.morphCrossSections()
 
     def fetchInputs(self):
 
@@ -62,20 +69,46 @@ class fit:
                 raise ValueError('No file found for tag {} and ecm {}'.format(tag,ecm))
             f = open('{}/{}'.format(self.input_dir,fl[0]), 'r')
             xsec.append(float(f.readlines()[0].split(',')[-1]))
-        df_xsec = pd.DataFrame({'ecm': self.l_ecm, 'xsec': xsec})
+        df_xsec = pd.DataFrame({'ecm': [float(ecm) for ecm in self.l_ecm], 'xsec': xsec})
         self.xsec_dict[tag] = df_xsec
             
     def readCrossSections(self):
         self.xsec_dict = {}
         for tag in self.l_tag:
             self.readScanPerTag(tag)
-        
+
+    def smearCrossSection(self,xsec):
+        if not self.smearXsec:
+            return xsec
+        xsec_to_smear = xsec[:-1] if self.l_ecm[-1] == '365.0' else xsec
+        xsec_smeared = convoluteXsecGauss(xsec_to_smear,self.beam_energy_res)
+        if self.l_ecm[-1] == '365.0':
+            xsec_smeared = pd.concat([xsec_smeared, xsec[-1:]])
+        return xsec_smeared
+
+    def smearCrossSections(self):
+        self.xsec_dict_smeared = {}
+        for tag in self.l_tag:
+            self.xsec_dict_smeared[tag] = self.smearCrossSection(self.xsec_dict[tag])
+
+    def morphCrossSection(self,param):
+        nominal = [self.param_dict['{}_nom'.format(p)] for p in self.params]
+        varied = [self.param_dict['{}_nom'.format(p) if p != param else '{}_var'.format(p)] for p in self.params]
+        xsec_nom = self.xsec_dict_smeared[formTag(*nominal)]
+        xsec_var = self.xsec_dict_smeared[formTag(*varied)]
+        df_morph = pd.DataFrame({'ecm': xsec_nom['ecm'], 'xsec': xsec_var['xsec']/xsec_nom['xsec'] -1})
+        return df_morph      
+
+    def morphCrossSections(self):
+        self.morph_dict = {}
+        for param in self.params:
+            self.morph_dict[param] = self.morphCrossSection(param)    
 
 
 def main():
     parser = argparse.ArgumentParser(description='Specify options')
-    parser.add_argument('--debug', action='store_true', help='Debug mode')
-
+    parser.add_argument('--debug', action='store_true', help='Debug mode') #does not do anything yet
+    
     f = fit(indir,parameters)
 
 
