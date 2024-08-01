cc36af2dff25a13653a27a6ac83e3aaaf572b1a6
Matteo Defranchis <matteo.defranchis@cern.ch>
2024-08-01 16:57:53 +0200

    minor fix

--- plotISR.py
+++ plotISR.py
@@ -5,7 +5,7 @@ import os
 from compareISR import convoluteXsecGauss
 
 smearing = True
-beam_energy_res = 0.23 # per beam, in percent
+beam_energy_res = 0.221 # per beam, in percent
 
 orders = ['NLO', 'NNLO', 'N3LO']
 #schemes = ['MS', 'PS']
@@ -31,7 +31,7 @@ def get_Xsec(order,scheme, mass='', yukawa='', width = '', alphaS='Nominal'):
     ecms = [round(ecm,1) for ecm in np.arange(335.0, 360.0, 0.1)]
     xsec = []
     for ecm in ecms:
-        f = open('output_ISR/{}_scan_{}_ISR_ecm{:.1f}_mass{}_width{}_yukawa{}_as{}'.format(order,scheme,ecm,mass,width,yukawa,alphaS), 'r')
+        f = open('output_ISR/no_grid/{}_scan_{}_ISR_ecm{:.1f}_mass{}_width{}_yukawa{}_as{}'.format(order,scheme,ecm,mass,width,yukawa,alphaS), 'r')
         xsec.append(float(f.readlines()[0].split(',')[-1]))
     df = pd.DataFrame({'ecm': ecms, 'xsec': xsec})
 
