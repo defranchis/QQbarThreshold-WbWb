2523c2de89030afb1f654c40132145b219309f78
Matteo Defranchis <matteo.defranchis@cern.ch>
2024-11-27 19:13:08 +0100

    new function for BEC corr

--- doFit.py
+++ doFit.py
@@ -555,7 +555,7 @@ class fit:
         fit_results = f_bec.getFitResults(printout=False)
         return [res.n for res in fit_results[:2]]
     
-
+    '''
     def doBECscanCorr(self):
         if not os.path.exists(indir_BEC):
             raise ValueError('Directory {} not found'.format(indir_BEC))
@@ -590,8 +590,9 @@ class fit:
         plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC.png')
         plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC.pdf')
         plt.clf()
+    '''
 
-
+    '''
     def doBECscanCorrMorph(self, min = -30, max = 30, points = 11):
 
         variations = np.linspace(min, max, points)
@@ -623,12 +624,61 @@ class fit:
         plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC_morph.png')
         plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC_morph.pdf')
         plt.clf()
-
+    '''
+    '''
     def fitBECvarMorph(self,var):
         f = copy.deepcopy(self)
         f.BEC_var_scenario *= (1 + var*np.array(self.morph_scenario['BEC']['xsec']))
         f.fitParameters(initMinuit=False)
         return [res.n for res in f.getFitResults(printout=False)[:2]]
+    '''
+
+    def doBECscanCorr(self, min = 0, max = 15, step = 1):
+        variations = np.arange(min,max+step/2,step)
+        l_mass = []
+        l_width = []
+        f = copy.deepcopy(self)
+        f.addBECnuisances(0)
+        for var in variations:
+            f.setBECpriors(prior_corr=var, prior_uncorr=0)
+            f.fitParameters(initMinuit=True)
+            fit_results = f.getFitResults(printout=False)
+            l_mass.append(fit_results[self.param_names.index('mass')].s)
+            l_width.append(fit_results[self.param_names.index('width')].s)
+
+        l_mass = np.array(l_mass)
+        l_width = np.array(l_width)
+
+        f.setBECpriors(prior_corr=uncert_BEC_default, prior_uncorr=0)
+        f.fitParameters(initMinuit=True)
+        fit_results = f.getFitResults(printout=False)
+        nominal_mass = fit_results[self.param_names.index('mass')].s
+        nominal_width = fit_results[self.param_names.index('width')].s
+
+        nominal_mass = (nominal_mass**2 - l_mass[0]**2)**.5
+        nominal_width = (nominal_width**2 - l_width[0]**2)**.5
+
+        l_mass = (l_mass**2 - l_mass[0]**2)**.5
+        l_width = (l_width**2 - l_width[0]**2)**.5
+
+
+        plt.plot(variations, l_mass*1E03, 'b-', label='Uncertainty in $m_t$', linewidth=2)
+        plt.plot(variations, l_width*1E03, 'g--', label='Uncertainty in $\Gamma_t$', linewidth=2)
+        plt.plot(uncert_BEC_default, (nominal_mass**2 - l_mass[0]**2)**.5*1E03, 'ro', label='Nominal fit', markersize=8)
+        plt.plot(uncert_BEC_default, (nominal_width**2 - l_width[0]**2)**.5*1E03, 'ro', label='', markersize=8)
+        plt.legend()
+        plt.title(r'$\mathit{{Preliminary}}$ ({:.0f} fb$^{{-1}}$)'.format(self.scenario_dict['total_lumi']/1E03), loc='right', fontsize=20)
+        plt.xlabel(r'Uncertainty in $\sqrt{s}$ [MeV]')
+        plt.ylabel('Impact on fitted parameter [MeV]')
+        offset = 0.1
+        plt.text(.9, 0.17 + offset, 'QQbar_Threshold N3LO+ISR', fontsize=23, transform=plt.gca().transAxes, ha='right')
+        plt.text(.9, 0.13 + offset, '[JHEP 02 (2018) 125]', fontsize=18, transform=plt.gca().transAxes, ha='right')
+        plt.text(.9, 0.08 + offset, '+ FCC-ee BES', fontsize=21, transform=plt.gca().transAxes, ha='right')
+        plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC_corr.png')
+        plt.savefig(plot_dir + '/uncert_mass_width_vs_BEC_corr.pdf')
+        plt.clf()
+
+        
     
     def addBECnuisances(self,prior):
         self.BEC_nuisances = True
@@ -658,9 +708,9 @@ class fit:
         l_mass = []
         l_width = []
         f = copy.deepcopy(self)
-        f.addBECnuisances()
+        f.addBECnuisances(0)
         for var in variations:
-            f.setBECprior(var)
+            f.setBECpriors(prior_uncorr=var, prior_corr=0)
             f.fitParameters(initMinuit=True)
             fit_results = f.getFitResults(printout=False)
             l_mass.append(fit_results[self.param_names.index('mass')].s)
@@ -1041,7 +1091,7 @@ def main():
         f.plotScaleVars() # to be implemented
     if args.BECscans:
         f.doBECscanCorr()
-        f.doBECscanCorrMorph()
+        #f.doBECscanCorrMorph()
         f.doBECscanUncorr()
     if args.BESscans:
         f.doBESscan()
