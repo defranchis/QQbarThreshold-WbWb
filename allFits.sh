e03a06454af8bb0abf2aaa8cd2c95923702502ff
Matteo Defranchis <matteo.defranchis@cern.ch>
2025-07-04 15:23:03 +0200

    helper scripts

---
+++ allFits.sh
@@ -0,0 +1,12 @@
+
+python doFit.py --pseudo --systTable
+python doFit.py --LSscan
+python doFit.py --lumiscan
+python doFit.py --alphaSscan
+python doFit.py --BECscan
+python doFit.py --BERscan
+python doFit.py --BESscan
+python doFit.py --chi2scan
+python doFit.py --scaleVars
+
+python doFit.py --systTable
---
+++ copy_plots_paper.sh
@@ -0,0 +1,4 @@
+mkdir -p plots/plots_paper
+cp plots/fit/*.pdf plots/plots_paper
+cp plots/plots_ISR/*.pdf plots/plots_paper
+cp plots/mt_mW_uncert.pdf plots/plots_paper
